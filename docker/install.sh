1d40f75431ca6096971dfd16bd1168d9533ef063
Xiao Kong <kongxiao0532@gmail.com>
2019-12-24 17:17:33 +0800

    Modify dockerfile

--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,7 @@ unzip llvmorg-9.0.0.zip
 cd llvm-project-llvmorg-9.0.0
 mkdir build
 cd build
-cmake -G 'Unix Makefiles'  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" ../llvm
+cmake -G 'Unix Makefiles'  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_BUILD_TYPE=MinSizeRel -LLVM_TARGETS_TO_BUILD=X86 ../llvm
 make -j8
 make install
 export PATH="/root/llvm-project-llvmorg-9.0.0/build/bin:$PATH"
